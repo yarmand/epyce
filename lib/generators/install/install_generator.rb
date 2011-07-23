29c7a0345202d93f4873b301655ed599f8db2af4
yann ARMAND <yann@harakys.com>
2011-07-24 01:25:31 +0200

    install generator from template

---
+++ lib/generators/install/install_generator.rb
@@ -0,0 +1,24 @@
+module Epyce
+  module Generators
+    class InstallGenerator < ::Rails::Generators::Base
+
+      desc "This generator install default haml application layout and several files in public"
+      source_root File.expand_path('../templates', __FILE__)
+
+      def copy_layout
+        say_status("Copying", "application layout files", :green)
+        ["application.html.haml", "_header.html.haml", "_footer.html.haml", "_flashes.html.haml"].each do |f|
+          copy_file "app/views/layouts/#{f}", "app/views/layouts/#{f}"
+        end
+      end
+
+      def copy_public
+        say_status("Copying", "application layout files", :green)
+        ["apple-touch-icon-114x114-precomposed.png", "apple-touch-icon-57x57-precomposed.png", "apple-touch-icon-72x72-precomposed.png", "apple-touch-icon-precomposed.png", "apple-touch-icon.png", "crossdomain.xml", "favicon.png" ].each do |f|
+          copy_file "public/#{f}", "public/#{f}"
+        end
+      end
+
+    end
+  end
+end
